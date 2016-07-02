0399df83479d0a37c1d63a845f483795b0e90715
Bert Vanderbauwhede <batlock666@gmail.com>
2016-07-02 20:01:31 +0200

    Solve file about_triangle_project.rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,13 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a == b and b == c
+    :equilateral
+  elsif a == b or b == c or c == a
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
